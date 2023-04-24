35133acb077700429fb43b32d4c6f196b736007a
Javier Gibert <adelgibert@gmail.com>
2023-04-24 12:37:26 +1200

    Added cards

--- src/components/showcase.js
+++ src/components/showcase.js
@@ -1,44 +1,62 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardDeck,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText,
+} from "reactstrap";
+
 // import { StaticImage } from "gatsby-plugin-image";
 import * as styles from "./showcase.module.css";
 
 const Showcase = () => {
   return (
-    <React.Fragment>
-      <Container className={styles.showcase}>
-        <Row>
+    <Container className={styles.container}>
+      <Row>
+        <Col>
           <h2>Fundamental Business Practices</h2>
-          <h4 className={styles.showcaseSubHeader}>
+          {/* <p className={styles.showcaseSubHeader}> */}
+          <p>
             Are you wantng to optimise you business and move it forward in an
             agile world?
-          </h4>
-        </Row>
-        <Row>
-          <Col>
-            🤨<h4>Book</h4>
-            <p>
-              Gain access to the demographics, psychographics, and location of
-              unique people.
-            </p>
-          </Col>
-          <Col>
-            😶‍🌫️<h4>Agility Transformation</h4>
-            <p>
-              Unify data from Facebook, Instagram, Twitter, LinkedIn, and
-              Youtube to gain rich insights.
-            </p>
-          </Col>
-          <Col>
-            😻<h4>Adaptive Strategy</h4>
-            <p>
-              Track actions taken on your website, understand the impact on your
-              bottom line.
-            </p>
-          </Col>
-        </Row>
-      </Container>
-    </React.Fragment>
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <CardDeck>
+            <Card className={styles.card}>
+              <CardBody>
+                <CardTitle>Book</CardTitle>
+                <CardText>
+                  Gain access to the demographics, psychographics, and location
+                  of unique people.
+                </CardText>
+              </CardBody>
+            </Card>
+          </CardDeck>
+        </Col>
+        <Col>
+          😶‍🌫️<h4>Agility Transformation</h4>
+          <p>
+            Unify data from Facebook, Instagram, Twitter, LinkedIn, and Youtube
+            to gain rich insights.
+          </p>
+        </Col>
+        <Col>
+          😻<h4>Adaptive Strategy</h4>
+          <p>
+            Track actions taken on your website, understand the impact on your
+            bottom line.
+          </p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
