8273d0f57014c380eb3182ea2da8f53fb8305f2c
Javier Gibert <adelgibert@gmail.com>
2023-04-24 12:42:42 +1200

    changed filename from Layout.js to layout.js

--- src/components/summary.js
+++ src/components/summary.js
@@ -6,31 +6,31 @@ import BookFeature from "./bookFeature";
 
 const Summary = () => {
   return (
-    <React.Fragment>
-      <Container className={styles.summary}>
-        <Row>
-          <Col>
-            {/* <h2>{`AGILITY MATTERS\nby Michael Law`}</h2> */}
-            <h2>AGILITY MATTERS</h2>
-            <h2>by Michael Law</h2>
-            <h4>{`Blurb of the book/ Overview on how it helps and what it covers.Buy it so you can be the best\nEtc etc Etc`}</h4>
-            <BookFeature />
-          </Col>
-          <Col>
-            <StaticImage
-              //   className={styles.heroImage}
-              src="../images/agility-matters-book-cover.png"
-              alt="Agility Matters Book Cover"
-              loading="eager"
-              width={364}
-              quality={95}
-              formats={["auto", "webp", "avif"]}
-              //   style={{ marginBottom: `var(--space-3)` }}
-            />
-          </Col>
-        </Row>
-      </Container>
-    </React.Fragment>
+    <Container className={styles.summary}>
+      <Row>
+        <Col>
+          {/* <h2>{`AGILITY MATTERS\nby Michael Law`}</h2> */}
+          <h2>AGILITY MATTERS</h2>
+          <h2>by Michael Law</h2>
+          <h4>{`Blurb of the book/ Overview on how it helps and what it covers.Buy it so you can be the best\nEtc etc Etc`}</h4>
+          {/* <BookFeature />
+          <BookFeature />
+          <BookFeature /> */}
+        </Col>
+        <Col>
+          <StaticImage
+            //   className={styles.heroImage}
+            src="../images/agility-matters-book-cover.png"
+            alt="Agility Matters Book Cover"
+            loading="eager"
+            width={364}
+            quality={95}
+            formats={["auto", "webp", "avif"]}
+            //   style={{ marginBottom: `var(--space-3)` }}
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
