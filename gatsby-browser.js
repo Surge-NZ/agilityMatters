3f901e613e00509318bbf92b73491db10a465f0a
Javier Gibert <adelgibert@gmail.com>
2023-04-26 19:50:52 +1200

    Analysis component styling improvements

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,4 @@ import "./src/assets/scss/now-ui-kit.scss?v=1.4.0"; //brings in NOWUI css from t
 //import "./src/assets/demo/react-demo.css?v=1.4.0"; //brings in react demo css from the location specified
 import "./src/assets/css/nucleo-icons-page-styles.css?v=1.4.0"; //brings in nucleo icons css from the location specified
 import "./src/assets/scss/index.scss"; //brings in our custom css from the location specified
+// import "./src/assets/css/now-ui-kit.css";
--- src/components/analysis.js
+++ src/components/analysis.js
@@ -1,13 +1,6 @@
 import React from "react";
-import {
-  Button,
-  Card,
-  CardBody,
-  CardTitle,
-  Container,
-  Row,
-  Col,
-} from "reactstrap";
+import { Button, Container, Row, Col } from "reactstrap";
+import * as styles from "./analysis.module.css";
 
 import { StaticImage } from "gatsby-plugin-image";
 
@@ -22,39 +15,46 @@ Right column with a centered image
 function Analysis() {
   return (
     <Container>
-      <Col>
-        <Card>
-          <CardBody>
-            <CardTitle>
-              <h2>Current State Analysis</h2>
-            </CardTitle>
-
-            <p>
-              A Current State Analysis is an important step in understanding
-              where your organisation currently stands in terms of agility. This
-              Analysis involves assessing current processes, systems, and
-              organisational structure to identify areas of inefficiency and
-              potential roadblocks to becoming more agile By Gaining a clear
-              understanding of the current state, you can create a comprehensive
-              plan for agility transformation that address the specific needs of
-              your organisation. Additionally, conducting a current state
-              analysis allows you to benchmark progress and measure the impact
-              of your transformation efforts over time. Overall, it is a key
-              understanding where you are now, so you can move forward where you
-              want to be.
+      <Row>
+        <Col>
+          <h2>
+            <b>Current State Analysis</b>
+          </h2>
+          <p>
+            A Current State Analysis is an important step in understanding where
+            your organisation currently stands in terms of agility. This
+            Analysis involves assessing current processes, systems, and
+            organisational structure to identify areas of inefficiency and
+            potential roadblocks to becoming more agile By Gaining a clear
+            understanding of the current state, you can create a comprehensive
+            plan for agility transformation that address the specific needs of
+            your organisation. Additionally, conducting a current state analysis
+            allows you to benchmark progress and measure the impact of your
+            transformation efforts over time. Overall, it is a key understanding
+            where you are now, so you can move forward where you want to be.
+          </p>
+          <blockquote className={styles.blockquote}>
+            <p className={styles.quoteText}>
+              For a free Current State analysis insight, please Utilise our
+              Interactive BAR to give you a Measure of your Organisation
             </p>
-          </CardBody>
-        </Card>
-      </Col>
-      <Col>
-        {" "}
-        <StaticImage
-          src="../images/current-state-analysis-graph.png"
-          alt="Current State Analysis Graph"
-          layout="fullWidth"
-          //   className={styles.backgroundImage}
-        />
-      </Col>
+          </blockquote>
+          <div className={styles.buttonContainer}>
+            <Button color="danger" className={styles.button}>
+              Free Analysis Tool
+            </Button>
+          </div>
+        </Col>
+        {/* // #F96332 color rectangle, text, button */}
+        <Col>
+          <StaticImage
+            src="../images/current-state-analysis-graph.png"
+            alt="Current State Analysis Graph"
+            layout="fullWidth"
+            //   className={styles.backgroundImage}
+          />
+        </Col>
+      </Row>
     </Container>
   );
 }
