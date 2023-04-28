e2e3d369fac5b3f6d807490696906fe5cb9b363d
Javier <javier@necta.nz>
2023-04-28 13:29:06 +1200

    Bunch of stuff I might finish but props not

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,25 @@ import React from "react";
 import Layout from "../components/layout";
 import Hero from "../components/hero";
 import Features from "../components/features";
+import TextImageLayout from "../components/TextImageLayout";
+import ImageComponent from "../components/mini-components/ImageComponent";
 // import Title from "../components/mini-components/title";
 
 const IndexPage = () => {
   return (
     <Layout>
-      <Hero />
-      <Features />
-      {/* <Title text="" /> */}
+      {/* <Hero /> */}
+      {/* <Features /> */}
+      <TextImageLayout
+        title="Default Title"
+        text="Default Text"
+        quote="Default Quote"
+        buttonText="Default Button Text"
+        image="https://picsum.photos/400/300
+        "
+        imageOnLeft={false}
+      />
+      <ImageComponent />
     </Layout>
   );
 };
