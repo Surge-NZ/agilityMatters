c26f953c0a69f0e965a7d7590a52ed8a73f0e18a
Javier <javier@necta.nz>
2023-05-03 17:18:49 +1200

    css

--- src/components/about.js
+++ src/components/about.js
@@ -1,59 +1,56 @@
-import React from "react"
-import { Container, Row, Col } from "reactstrap"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { useStaticQuery, graphql } from "gatsby";
 
 const About = () => {
-
-  const items = 
-    {
-      fileName: "mike",
-      altText: "hi",
-    }
-  ;
-
-    const data = useStaticQuery(graphql`
-      query {
-        allFile {
-          nodes {
-            name
-            childImageSharp {
-              gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
-            }
+  const items = {
+    fileName: "mike",
+    altText: "hi",
+  };
+  //
+  const data = useStaticQuery(graphql`
+    query {
+      allFile {
+        nodes {
+          name
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
           }
         }
       }
-    `);
+    }
+  `);
 
-      const getImageByName = (name) => {
-          return data.allFile.nodes.find((node) => node.name === name);
-            };
-
-      const imageData = getImageByName(items.fileName);
-      const image = getImage(imageData);
+  // 3rd
+  const getImageByName = (name) => {
+    return data.allFile.nodes.find((node) => node.name === name);
+  };
+  // 1st
+  const imageData = getImageByName(items.fileName);
+  // 2nd
+  const image = getImage(imageData);
 
   return (
     <div className="cd-section" id="features">
-        <div className="features-1">
-          <Container>
-            <Row>
-              <Col className="ml-auto mr-auto" md="4">
-                <GatsbyImage
-                  image={image}
-                  alt={items.altText}
-                  className="d-block img-raised"
-                />
-              </Col>
-              <Col className="ml-auto" md="6">
-
-      <h1>ABOUT</h1>
-              </Col>
-            </Row>
-          </Container>
-        </div>
+      <div className="features-1">
+        <Container>
+          <Row>
+            <Col className="ml-auto mr-auto" md="4">
+              <GatsbyImage
+                image={image}
+                alt={items.altText}
+                className="d-block img-raised"
+              />
+            </Col>
+            <Col className="ml-auto" md="6">
+              <h1>ABOUT</h1>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
-
+export default About;
