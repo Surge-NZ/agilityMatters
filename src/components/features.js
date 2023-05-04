5e2425defc56a18c8371f94920467e9cc6263f87
bonmaklad <michael@acv.co.nz>
2023-05-04 16:53:22 +1200

    finished

--- src/components/features.js
+++ src/components/features.js
@@ -22,10 +22,15 @@ function Features() {
           <Container>
             <Row>
               <Col className="ml-auto mr-auto" md="12">
-                <h2 className="title">Fundamental Business Practices</h2>
+                <h2 className="title">About This Book</h2>
                 <h4 className="description">
-                  Are you wanting to optimise your business and move it forward
-                  in an agile world?
+                Join Julie, a new GM, as she takes on the challenge of transforming NZFS, a company plagued by the all-too-familiar issues that afflict large organisations. 
+
+Drawing upon a wealth of real-life case studies, this captivating story follows Julie as she measures the company’s current state, identifies pain points, and enhances customer value. With each iteration, she brings her department closer to success, proving that change is indeed possible.
+
+Blending fiction with the author’s firsthand encounters, this compelling tale offers not only a gripping narrative but also the academic foundation that explains why these pain points arise and how they can be resolved. This unique combination of storytelling and practical insight will keep you hooked from the first page to the last.
+
+Will Julie’s pursuit of change usher in a new era for NZFS?
                 </h4>
               </Col>
             </Row>
@@ -35,10 +40,9 @@ function Features() {
                   <div className="icon icon-primary">
                     <i className="now-ui-icons ui-2_chat-round"></i>
                   </div>
-                  <h4 className="info-title">Book</h4>
+                  <h4 className="info-title">Current State Analysis</h4>
                   <p className="description">
-                    Gain access to the demographics, psychographics, and
-                    location of unique people.
+                    Leveraging the business agility radar to understand your organisations strengths and limitations.
                   </p>
                 </div>
               </Col>
@@ -47,10 +51,9 @@ function Features() {
                   <div className="icon icon-primary">
                     <i className="now-ui-icons business_chart-pie-36"></i>
                   </div>
-                  <h4 className="info-title">Agility Transformation</h4>
+                  <h4 className="info-title">Adaptive Strategy</h4>
                   <p className="description">
-                    Unify data from Facebook, Instagram, Twitter, LinkedIn, and
-                    Youtube to gain rich insights.
+                    Move from strategic planning to strategic intent with the ability to dynamically change direction.
                   </p>
                 </div>
               </Col>
@@ -59,10 +62,9 @@ function Features() {
                   <div className="icon icon-primary">
                     <i className="now-ui-icons design-2_ruler-pencil"></i>
                   </div>
-                  <h4 className="info-title">Adaptive Strategy</h4>
+                  <h4 className="info-title">Customer Value</h4>
                   <p className="description">
-                    Track actions taken on your website, understand the impact
-                    on your bottom line.
+                    Focus your organisation on value consumed by the customer by reducing waste and focusing on the right metrics.
                   </p>
                 </div>
               </Col>
--- src/components/paragraph-text.js
+++ src/components/paragraph-text.js
@@ -3,8 +3,7 @@ import React from "react";
 function ParagraphText() {
   const text = `Join Julie, a new General Manager, as she takes on the challenge of transforming NZFS, a company plagued by the all-too-familiar issues that afflict large organisations. 
 
-  Drawing upon a wealth of real-life case studies, this 
-  captivating story follows Julie as she measures the company’s current state, identifies pain points, and enhances customer value. With each iteration, she brings her department closer to success, proving that change is indeed possible.
+  Drawing upon a wealth of real-life case studies, this captivating story follows Julie as she measures the company’s current state, identifies pain points, and enhances customer value. With each iteration, she brings her department closer to success, proving that change is indeed possible.
   
   Blending fiction with the author’s firsthand encounters, this compelling tale offers not only a gripping narrative but also the academic foundation that explains why these pain points arise and how they can be resolved. This unique combination of storytelling and practical insight will keep you hooked from the first page to the last.
   
