7452ae9d845f42e9590339384ea2b09b8b534920
Javier <javier@necta.nz>
2023-05-05 10:04:01 +1200

    spelling bugs fixed

--- src/components/features.js
+++ src/components/features.js
@@ -8,29 +8,36 @@ import * as styles from "./hero.module.css";
 // core components
 // import VideoComponent from "./video-component";
 
-
-
 function Features() {
   return (
     <>
-      <div className="cd-section" id="features"
+      <div className="cd-section" id="features">
+        <div
+          className="features-1"
+          style={{
+            padding: "50px 0px 0px 0px",
+          }}
         >
-        <div className="features-1"
-        style={{
-          padding: "50px 0px 0px 0px",
-        }}>
           <Container>
             <Row>
               <Col className="ml-auto mr-auto" md="12">
                 <h2 className="title">About This Book</h2>
                 <h4 className="description">
-                Join Julie, a new GM, as she takes on the challenge of transforming NZFS, a company plagued by the all-too-familiar issues that afflict large organisations. 
-
-Drawing upon a wealth of real-life case studies, this captivating story follows Julie as she measures the company’s current state, identifies pain points, and enhances customer value. With each iteration, she brings her department closer to success, proving that change is indeed possible.
-
-Blending fiction with the author’s firsthand encounters, this compelling tale offers not only a gripping narrative but also the academic foundation that explains why these pain points arise and how they can be resolved. This unique combination of storytelling and practical insight will keep you hooked from the first page to the last.
-
-Will Julie’s pursuit of change usher in a new era for NZFS?
+                  Join Julie, a new GM, as she takes on the challenge of
+                  transforming NZFS, a company plagued by the all-too-familiar
+                  issues that afflict large organisations. Drawing upon a wealth
+                  of real-life case studies, this captivating story follows
+                  Julie as she measures the company’s current state, identifies
+                  pain points, and enhances customer value. With each iteration,
+                  she brings her department closer to success, proving that
+                  change is indeed possible. Blending fiction with the author’s
+                  firsthand encounters, this compelling tale offers not only a
+                  gripping narrative but also the academic foundation that
+                  explains why these pain points arise and how they can be
+                  resolved. This unique combination of storytelling and
+                  practical insight will keep you hooked from the first page to
+                  the last. Will Julie’s pursuit of change usher in a new era
+                  for NZFS?
                 </h4>
               </Col>
             </Row>
@@ -42,7 +49,8 @@ Will Julie’s pursuit of change usher in a new era for NZFS?
                   </div>
                   <h4 className="info-title">Current State Analysis</h4>
                   <p className="description">
-                    Leveraging the business agility radar to understand your organisations strengths and limitations.
+                    Leveraging the business agility radar to understand your
+                    organisations strengths and limitations.
                   </p>
                 </div>
               </Col>
@@ -53,7 +61,8 @@ Will Julie’s pursuit of change usher in a new era for NZFS?
                   </div>
                   <h4 className="info-title">Adaptive Strategy</h4>
                   <p className="description">
-                    Move from strategic planning to strategic intent with the ability to dynamically change direction.
+                    Move from strategic planning to strategic intent with the
+                    ability to dynamically change direction.
                   </p>
                 </div>
               </Col>
@@ -64,7 +73,8 @@ Will Julie’s pursuit of change usher in a new era for NZFS?
                   </div>
                   <h4 className="info-title">Customer Value</h4>
                   <p className="description">
-                    Focus your organisation on value consumed by the customer by reducing waste and focusing on the right metrics.
+                    Focus your organisation on value consumed by the customer by
+                    reducing waste and focusing on the right metrics.
                   </p>
                 </div>
               </Col>
@@ -92,7 +102,13 @@ Will Julie’s pursuit of change usher in a new era for NZFS?
                     <div className="description">
                       <h4 className="info-title">Business Agility Radar</h4>
                       <p>
-                      Adapting an organisation to respond to customer needs in a flexible way drives better business outcomes. Agility allows organisations to prospere by focusing on Customer Value. The Business Agility Radar allows your organisation to measure the culture across the four values: Adaptive Strategy, Cognitive Diversity, Customer-Centricity and Leadership and Employee Empowerment. 
+                        Adapting an organisation to respond to customer needs in
+                        a flexible way drives better business outcomes. Agility
+                        allows organisations to prosper by focusing on Customer
+                        Value. The Business Agility Radar allows your
+                        organisation to measure their culture across four
+                        values: Adaptive Strategy, Cognitive Diversity,
+                        Customer-Centricity and Employee Empowerment.
                       </p>
                     </div>
                   </div>
@@ -101,9 +117,16 @@ Will Julie’s pursuit of change usher in a new era for NZFS?
                       {/* <i className="now-ui-icons sport_user-run"></i> */}
                     </div>
                     <div className="description">
-                      <h4 className="info-title">Amber to Orange Delivery Model</h4>
+                      <h4 className="info-title">
+                        Amber to Orange Delivery Model
+                      </h4>
                       <p>
-                      Our research has shown that organisations that are Amber seem to be the most common, those organisations focus on efficientcy and their products, we have developed a framework that enables those organisations to move to the next stage, which is an outcome focussed delivery model. 
+                        Our research has shown that organisations that are
+                        'Amber' seem to be the most common; those organisations
+                        focus on efficiency and their products, we have
+                        developed a framework that enables those organisations
+                        to move to the next stage, which is an outcome focused
+                        delivery method.
                       </p>
                     </div>
                   </div>
@@ -112,43 +135,48 @@ Will Julie’s pursuit of change usher in a new era for NZFS?
                       {/* <i className="now-ui-icons ui-2_time-alarm"></i> */}
                     </div>
                     <div className="description">
-                      <h4 className="info-title">7 Circumstances of Strategy</h4>
+                      <h4 className="info-title">
+                        7 Circumstances of Strategy
+                      </h4>
                       <p>
-                      Seven simple questions to ask. WHY do you exsist, WHO do you serve, WHAT pain pont do you solve, HOW do you solve it, IN WHAT WAY will you deliver and WHEN. When one questions changes, the whole strategy changes below.
+                        Seven simple questions to ask; WHY do you exist? WHO do
+                        you serve? WHAT Pain Point do you solve? HOW do you
+                        solve it? IN WHAT WAY will you deliver and WHEN? When
+                        one question changes, the whole strategy changes and
+                        adapts.
                       </p>
                     </div>
                   </div>
                 </Col>
-                <Col lg="6" className="tablet-container d-flex align-items-center" >
-                  
-                    <StaticImage
-                      // className={styles.heroImage}
-                      src="../images/1682629796.png"
-                      alt="Agility Matters Book Cover"
-                      loading="eager"
-                      objectFit="contain"
-                      // width={1500}
-                      quality={100}
-                      placeholder="blurred"
-                      formats={["auto", "webp", "avif"]}
-                     
-                    />
+                <Col
+                  lg="6"
+                  className="tablet-container d-flex align-items-center"
+                >
+                  <StaticImage
+                    // className={styles.heroImage}
+                    src="../images/1682629796.png"
+                    alt="Agility Matters Book Cover"
+                    loading="eager"
+                    objectFit="contain"
+                    // width={1500}
+                    quality={100}
+                    placeholder="blurred"
+                    formats={["auto", "webp", "avif"]}
+                  />
                 </Col>
-                  
-                  
+
                 <Row>
                   <Col>
-                  <Button
-                    className={styles.heroButton}
-                    aria-label="Coming Soon"
-                    href="https://www.amazon.com/"
-                  >
-                    COMING SOON
-                  </Button>
+                    <Button
+                      className={styles.heroButton}
+                      aria-label="Coming Soon"
+                      href="https://www.amazon.com/"
+                    >
+                      COMING SOON
+                    </Button>
                   </Col>
                 </Row>
               </Row>
-         
             </Container>
           </div>
         </div>
--- src/components/praise.js
+++ src/components/praise.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "gatsby"
+import Link from "gatsby";
 
 // reactstrap components
 import { Button, Container, Row, Col } from "reactstrap";
@@ -9,40 +9,38 @@ import * as styles from "./hero.module.css";
 // import VideoComponent from "./video-component";
 import Agil from "../assets/video/agil.mp4";
 
-
-
 function Praise() {
   return (
-   
-<div className="section">
-<div className="about-description text-center">
-<div className={styles.videoAndTestimonials}>
-<Container>
-<Row>
-<h2 className="title" 
-  style={{ 
-    textAlign: "center"
-  }}>Praise for Agility Matters</h2>
-<Col lg={6} className="d-flex align-items-center" > 
+    <div className="section">
+      <div className="about-description text-center">
+        <div className={styles.videoAndTestimonials}>
+          <Container>
+            <Row>
+              <h2
+                className="title"
+                style={{
+                  textAlign: "center",
+                }}
+              >
+                Praise for Agility Matters
+              </h2>
+              <Col lg={6} className="d-flex align-items-center">
+                <video controls className={styles.video}>
+                  <source src={Agil} type="video/mp4" loading="lazy" />
+                </video>
+              </Col>
 
-
-    <video controls className={styles.video}>
-      <source src={Agil} type="video/mp4" loading="lazy" />
-    </video>
-
-  
-    
- 
-</Col>
-
-<Col lg={6}>
-
-  <div className="typography-line">
-                  
+              <Col lg={6}>
+                <div className="typography-line">
                   <blockquote>
-                    <p className="blockquote blockquote" 
-                    >
-                      "This is an easy to read book. It has lots of practical techniques you can use with explanations of how it was used in the story to achieve an outcome.  This book gets you excited about the opportunities for improvement within your own company. If NZFS can do it. Surely we can too.   It also includes the academic details to back up the practices and appeal to those that need more detail."{" "}
+                    <p className="blockquote blockquote">
+                      "This is an easy to read book. It has lots of practical
+                      techniques you can use with explanations of how it was
+                      used in the story to achieve an outcome. This book gets
+                      you excited about the opportunities for improvement within
+                      your own company. If NZFS can do it. Surely we can too. It
+                      also includes the academic details to back up the
+                      practices and appeal to those that need more detail."{" "}
                       <br></br>
                       <br></br>
                       <small>- Nicole Arthur</small>
@@ -50,36 +48,39 @@ function Praise() {
                   </blockquote>
 
                   <blockquote>
-                    <p className="blockquote blockquote" 
-                    >
-                      "This book is a valuable resource for anyone interested in implementing customer-focussed agility practices and other organisational theories in the workplace. Written from the perspective of Julie, the new Technology Manager at the fictional NZFS, it provides practical descriptions and scenarios which will undoubtably be familiar to anyone who has worked in a large company. Highly recommended for anyone looking to improve their customer engagement and streamline their work processes."{" "}
-                      <br></br>
+                    <p className="blockquote blockquote">
+                      "This book is a valuable resource for anyone interested in
+                      implementing customer-focused agility practices and other
+                      organisational theories in the workplace. Written from the
+                      perspective of Julie, the new Technology Manager at the
+                      fictional NZFS, it provides practical descriptions and
+                      scenarios which will undoubtably be familiar to anyone who
+                      has worked in a large company. Highly recommended for
+                      anyone looking to improve their customer engagement and
+                      streamline their work processes." <br></br>
                       <br></br>
                       <small>- Rachel Astruc</small>
                     </p>
                   </blockquote>
-               
-  </div>
-
-
-</Col>
-</Row>
-<Row className="tablet-container d-flex align-items-center">
-      <Col>
-  <Button
-    className={styles.heroButton}
-    aria-label="Coming Soon"
-    href="https://www.youtube.com/playlist?list=PL7Tt7ju84Q_zWRG05UzGLzr_wQAcI9T-0"
-  >
-    WATCH MORE
-  </Button>
-  </Col>
-  </Row>
-</Container>
-</div>
-</div>
-</div>
-);
+                </div>
+              </Col>
+            </Row>
+            <Row className="tablet-container d-flex align-items-center">
+              <Col>
+                <Button
+                  className={styles.heroButton}
+                  aria-label="Coming Soon"
+                  href="https://www.youtube.com/playlist?list=PL7Tt7ju84Q_zWRG05UzGLzr_wQAcI9T-0"
+                >
+                  WATCH MORE
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Praise;
