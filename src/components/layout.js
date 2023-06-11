bca2774054403a97d968e4bc09d6bbef82d9a71b
jnecta <javier@necta.nz>
2023-06-12 11:45:15 +1200

    incomplete "necta" nav

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./navbar";
+import Nav from "./nav";
 
 const Layout = ({ children }) => {
   return (
     <React.Fragment>
       <Navbar />
+      <Nav />
       {children}
     </React.Fragment>
   );
