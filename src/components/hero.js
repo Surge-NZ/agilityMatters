10195ce58b3319eadec6219449ceb2633aa6215f
jnecta <javier@necta.nz>
2023-06-12 14:02:25 +1200

    commented out blog due to errors

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,7 +7,6 @@ import * as styles from "./hero.module.css";
 const Hero = () => {
   return (
     <div className={styles.hero}>
-   
       <Row>
         <Col md={6} className={styles.heroLeftColumn}>
           <StaticImage
@@ -31,20 +30,26 @@ const Hero = () => {
             flexibly.
           </p>
           <div className="d-flex justify-content-center mt-3">
-
-          <Button className={styles.heroButton} aria-label="purchase" href="https://www.amazon.com.au/Agility-Matters-Adapting-Organisation-Flexibility-ebook/dp/B0C5FG4FN1/ref=sr_1_1?crid=10QMLX1XEGHB1&keywords=agility+matters&qid=1684815247&sprefix=agility+matter%2Caps%2C326&sr=8-1">
-            GET A COPY HERE
-          </Button>
-          {/* note, inconsistency in styles: first button is in all caps, second button is camel case. Advising a higher font weight and better color contrast for better readibility -J */}
-          <Button className={styles.heroButton} aria-label="messure-current-state" href="https://agility.scoreapp.com/" 
-          style={{ backgroundColor: "#696969", color: 'yellow' }}>
-            Measure Current State
-          </Button>
+            <Button
+              className={styles.heroButton}
+              aria-label="purchase"
+              href="https://www.amazon.com.au/Agility-Matters-Adapting-Organisation-Flexibility-ebook/dp/B0C5FG4FN1/ref=sr_1_1?crid=10QMLX1XEGHB1&keywords=agility+matters&qid=1684815247&sprefix=agility+matter%2Caps%2C326&sr=8-1"
+            >
+              GET A COPY HERE
+            </Button>
+            {/* note, inconsistency in styles: first button is in all caps, second button is camel case. Advising a higher font weight and better color contrast for better readibility -J */}
+            <Button
+              className={styles.heroButton}
+              aria-label="messure-current-state"
+              href="https://agility.scoreapp.com/"
+              style={{ backgroundColor: "#696969", color: "yellow" }}
+            >
+              Measure Current State
+            </Button>
           </div>
         </Col>
       </Row>
-      </div>
-  
+    </div>
   );
 };
 
