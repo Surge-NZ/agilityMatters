3b1ad07053805c6c3ab9bec4602c07b2c06bde54
Javier <javier@necta.nz>
2023-06-22 15:50:08 +1200

    applied reset file

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "./src/components/layout";
+import "./src/assets/css/reset.css"; // reset styles
 import "@fontsource-variable/montserrat"; //  bring in montserrat font from fontsource
 import "bootstrap/dist/css/bootstrap.min.css"; //brings in bootstrap css from the location specified
 import "./src/assets/scss/now-ui-kit.scss?v=1.4.0"; //brings in NOWUI css from the location specified
@@ -9,5 +10,5 @@ import "./src/assets/demo/nucleo-icons-page-styles.css?v=1.4.0"; //brings in nuc
 import "./src/assets/css/nucleo-icons-page-styles.css?v=1.4.0"; //brings in nucleo icons css from the location specified
 import "./src/assets/scss/index.scss"; //brings in our custom css from the location specified
 
-// this wraps all pages with layout, regardless if its called in the page itself
+// this wraps all pages with layout, regardless if layout is called in the page itself
 export const wrapPageElement = ({ element }) => <Layout>{element}</Layout>;
